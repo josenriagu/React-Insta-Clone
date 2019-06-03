80a5b33dc95179319884b07a168ba8528e72b07d
Josemaria <josemarianriagu25@gmail.com>
2019-06-04 01:04:21 +0800

    Create PostContainer component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Icons from '../SearchBar/Icons';
 import './PostContainer.css';
 
 const PostContainer = props => {
-
+    return (
+        <article className="post-container">
+            <div className="post">
+                <div className="post-head">
+                    <div className="profile-image">
+                        <img src="#" alt="mini profile" />
+                    </div>
+                    <div className="username">Philzcofee</div>
+                </div>
+                <div className="post-image">
+                    <img src="#" alt="post" />
+                </div>
+                <Icons />
+            </div>
+        </article>
+    );
 }
 
 export default PostContainer;
