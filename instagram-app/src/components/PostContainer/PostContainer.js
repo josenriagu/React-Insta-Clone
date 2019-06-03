86cc949d070bdf6bc3caad239363724a067ce9cb
Josemaria <josemarianriagu25@gmail.com>
2019-06-03 21:08:54 +0800

    Create component files

---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PostContainer.css';
+
+const PostContainer = props => {
+
+}
+
+export default PostContainer;
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = props => {
+
+}
+
+export default SearchBar;
