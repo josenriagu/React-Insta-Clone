2b0075fef1de0d3dea0a1e287c29ee232466f20a
Josemaria <josemarianriagu25@gmail.com>
2019-06-03 23:05:34 +0800

    Import SearchBar into App

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import CommentSection from './components/CommentSection/CommentSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
     </div>
   );
 }
