7880bf87d86c4a52794ce60d29c8f85c524ea2a8
Josemaria <josemarianriagu25@gmail.com>
2019-06-05 06:10:15 +0800

    Refactor CommentSection component

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import Comment from './Comment';
+import AddComment from '../CommentSection/AddComment';
 import './CommentSection.css';
 
-const CommentSection = ({ id, username, text }) => {
-    
-    return (
-        <div id={id} className="comment">
-            <p><strong>{username} </strong>{text}</p>
-        </div>
-        
-    );
-}
+export default class CommentSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: this.props.comments,
+            commentText: ""
+        }
+    }
 
-export default CommentSection;
+    changeHandler = event => {
+        this.setState({ ...this.state, commentText: event.target.value });
+        console.log("sfgsg")
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        this.props.addComment(this.props.id, this.state.commentText)
+        this.setState({ ...this.state, commentText: "" })
+    }
+
+    render() {
+        const { id, timestamp, comments } = this.props;
+        // tweaks with moment.js library
+        // let dt = moment(timestamp, ["MMMM Do YYYY, h:mm:ss a"]).format("HH:mm");
+        // let dtt =moment().startOf(timestamp).fromNow();
+        let new_timestamp = moment(timestamp, "MMMM Do YYYY, h:mm:ss a").fromNow();
+        return (
+            <div id={id} className="comment-section">
+                {
+                    comments.map(comment => {
+                        return (
+                            <Comment
+                                id={comment.id}
+                                username={comment.username}
+                                text={comment.text}
+                            />
+                        );
+                    })
+                }
+                <div className="post-time">{new_timestamp}</div>
+                <div className="post-footer">
+                    <AddComment
+                        changeHandler={this.changeHandler}
+                        submitHandler={this.submitHandler}
+                        commentText={this.state.commentText}
+                    />
+                </div>
+            </div>
+
+        );
+    }
+}
