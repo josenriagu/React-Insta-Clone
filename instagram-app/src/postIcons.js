79a629323cb1c2b40a8b271e638d73dccd464edf
Josemaria <josemarianriagu25@gmail.com>
2019-06-04 16:48:44 +0800

    Add share icon

--- instagram-app/src/postIcons.js
+++ instagram-app/src/postIcons.js
@@ -1,5 +1,5 @@
 import uuid from 'uuid';
 
-const postIcons = [{ id: uuid(), src: "/img/icons/like.png" },{ id: uuid(), src: "/img/icons/chat.png" }];
+const postIcons = [{ id: uuid(), src: "/img/icons/like.png" },{ id: uuid(), src: "/img/icons/chat.png" }, { id: uuid(), src: "/img/icons/share.png" }];
 
 export default postIcons;
