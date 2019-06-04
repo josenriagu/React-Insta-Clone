86525f3c44d8f6ea5b2bb7499dbf0858d1f2412c
Josemaria <josemarianriagu25@gmail.com>
2019-06-04 12:19:43 +0800

    Create CommentSection component

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './CommentSection.css';
 
-const CommentSection = props => {
-
+const CommentSection = ({ id, username, text }) => {
+    
+    return (
+        <div id={id} className="comment">
+            <p><strong>{username} </strong>{text} <span>more</span></p>
+        </div>
+        
+    );
 }
 
 export default CommentSection;
