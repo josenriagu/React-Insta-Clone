d354f69ceb9abe19b0a5917b5b668947eb9935fe
Josemaria <josemarianriagu25@gmail.com>
2019-06-04 12:17:01 +0800

    Pass props to Icons component

--- instagram-app/src/components/SearchBar/Icons.js
+++ instagram-app/src/components/SearchBar/Icons.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './SearchBar.css';
 
-const Icons = props => {
+const Icons = ({src}) => {
     return (
-        <div className="icon-holder">
-            <div className="icon">
-                <img src="/img/icons/explore.png" alt="logo" />
-            </div>
-            <div className="icon">
-                <img src="/img/icons/like.png" alt="logo" />
-            </div>
-            <div className="icon">
-                <img src="/img/icons/user.png" alt="logo" />
-            </div>
+        <div className="icon">
+            <img src={src} alt="logo" />
         </div>
     );
 }
