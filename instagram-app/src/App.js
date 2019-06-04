2e54238316bf147eebdc073119380d0fdcd1b5df
Josemaria <josemarianriagu25@gmail.com>
2019-06-05 02:54:07 +0800

    Define save to localStorage method Define searchPost method

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,40 +1,62 @@
 import React, { Component } from 'react';
-import dummyData from './dummy-data';
-import searchIcons from './searchIcons';
-import postIcons from './postIcons';
+import dummyData, { searchIcons, postIcons } from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 import ProfileSection from './components/ProfileSection/ProfileSection';
 import './App.css';
 
+// Variable assignments
+let postData = dummyData;
+const localStorage = window.localStorage;
+
+if (localStorage.getItem("posts-saved")) {
+  // initialize saving to local storage
+  postData = JSON.parse(localStorage.getItem("posts-saved"));
+} else {
+  localStorage.setItem("posts-saved", JSON.stringify(postData));
+}
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postData: postData,
+      newComment: "",
+      searchString: ""
+    }
+  }
+
+
+  savePosts = () => {
+    localStorage.setItem("posts-saved", JSON.stringify(this.state.postData));
+  };
+
+  searchPosts = event => {
+    this.setState({
+      searchString: event.target.value
+    });
+  };
+
   render() {
+    // Call save post method on render to save current state
+    this.savePosts();
     return (
       <div className="App">
         <SearchBar
+          value={this.state.searchString}
+          changeHandler={this.searchPosts}
           searchIcons={searchIcons}
         />
         <main className="main">
           <PostContainer
-          dummyData={dummyData}
-          postIcons={postIcons}
+            searchString={this.state.searchString}
+            postData={this.state.postData}
+            postIcons={postIcons}
           />
           <ProfileSection />
         </main>
-        
+
       </div>
     );
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <SearchBar />
-//       <PostContainer />
-//     </div>
-//   );
-// }
-
-// export default App;
