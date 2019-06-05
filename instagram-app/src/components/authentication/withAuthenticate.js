7d8699a963a1d1464a8d69116994d32f24a54395
Josemaria <josemarianriagu25@gmail.com>
2019-06-06 04:12:53 +0800

    Update withAuthenticate to use state Implement double arrow functionality

--- instagram-app/src/components/authentication/withAuthenticate.js
+++ instagram-app/src/components/authentication/withAuthenticate.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
-const withAuthenticate = App => {
+const withAuthenticate = App => Login => {
     return class extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                loggedIn: false,
+            }
+        }
+        componentDidMount() {
+            if (localStorage.getItem("username")) {
+                this.setState({ loggedIn: true })
+            }
+        }
         render() {
-            return <App {...this.props} />;
+            if (this.state.loggedIn) {
+                return <App {...this.props} />;
+            }
+            return <Login />
         }
     };
 }
