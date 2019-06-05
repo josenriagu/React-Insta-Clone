c4c1252be358bc557f827625cc1a6c87787daea0
Josemaria <josemarianriagu25@gmail.com>
2019-06-05 15:18:51 +0800

    Define updateLike method

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -1,48 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icons from '../SearchBar/Icons';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
 
-const Post = ({ id, username, likes, timestamp, comments, thumbnailUrl, imageUrl, postIcons, addComment }) => {
-    return (
-        <div id={id} className="post">
-            <div className="post-head">
-                <div className="left">
-                    <div className="profile-thumbnail small">
-                        <img src={thumbnailUrl} alt="profile thumbnail" />
+export default class Post extends Component {
+    updateLike = () => {
+        this.props.addLike(this.props.id)
+    }
+    render() {
+        const { id, username, likes, timestamp, comments, thumbnailUrl, imageUrl, postIcons, addComment } = this.props
+        return (
+            <div id={id} className="post">
+                <div className="post-head">
+                    <div className="left">
+                        <div className="profile-thumbnail small">
+                            <img src={thumbnailUrl} alt="profile thumbnail" />
+                        </div>
+                        <div className="profile-details username"><strong>{username}</strong></div>
                     </div>
-                    <div className="profile-details username"><strong>{username}</strong></div>
+                    <div className="right"><strong>∙ ∙ ∙</strong></div>
                 </div>
-                <div className="right"><strong>∙ ∙ ∙</strong></div>
-            </div>
-            <div className="post-image">
-                <img src={imageUrl} alt="post" />
-            </div>
-            <div className="post-body">
-                <div className="post-interaction">
-                    <div className="icons">
-                        {
-                            postIcons.map(icon => {
-                                return (
-                                    <Icons
-                                    key={icon.id}
-                                    src={icon.src}
-                                    />
-                                );
-                            })
-                        }
+                <div className="post-image">
+                    <img src={imageUrl} alt="post" />
+                </div>
+                <div className="post-body">
+                    <div className="post-interaction">
+                        <div className="icons">
+                            {
+                                postIcons.map(icon => {
+                                    return (
+                                        <Icons
+                                            key={icon.id}
+                                            id={id}
+                                            src={icon.src}
+                                            updateLike={this.updateLike}
+                                        />
+                                    );
+                                })
+                            }
+                        </div>
+                        <p><strong>{likes} likes</strong></p>
                     </div>
-                    <p><strong>{likes} likes</strong></p>
                 </div>
+                <CommentSection
+                    id={id}
+                    comments={comments}
+                    timestamp={timestamp}
+                    addComment={addComment}
+                />
             </div>
-            <CommentSection
-                id={id}
-                comments={comments}
-                timestamp={timestamp}
-                addComment={addComment}
-            />
-        </div>
-    );
+        );
+    }
 }
-
-export default Post;
