da93a07b4793fbad3da973a217ac0688259bb8bf
Josemaria <josemarianriagu25@gmail.com>
2019-06-06 00:30:14 +0800

    Refactor App render into PostsPage

---
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import PostContainer from './PostContainer';
+import ProfileSection from '../ProfileSection/ProfileSection';
+
+const PostsPage = ({value, changeHandler, searchIcons, searchString, postData, postIcons, addComment, addLike }) => {
+    return (
+        <div className="posts-page">
+            <SearchBar
+                value={value}
+                changeHandler={changeHandler}
+                searchIcons={searchIcons}
+            />
+            <main className="main">
+                <PostContainer
+                    searchString={searchString}
+                    postData={postData}
+                    postIcons={postIcons}
+                    addComment={addComment}
+                    addLike={addLike}
+                />
+                <ProfileSection />
+            </main> 
+        </div>
+    );
+}
+
+export default PostsPage;
