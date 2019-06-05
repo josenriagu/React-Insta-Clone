c7dd321ffdd3b6d7318e03d770e99cb1c3023ad1
Josemaria <josemarianriagu25@gmail.com>
2019-06-05 19:06:39 +0800

    Refactor to selectively use click event

--- instagram-app/src/components/SearchBar/Icons.js
+++ instagram-app/src/components/SearchBar/Icons.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './SearchBar.css';
 
-const Icons = ({ src, updateLike}) => {
+const Icons = ({ src, name, updateLike }) => {
+    if (name === "like") {
+        return (
+            <div
+                className="icon"
+                onClick={(event) => updateLike(event)}
+            >
+                <img src={src} alt="icon" />
+            </div>)
+    }
     return (
-        <div onClick={(event) => updateLike(event)} className="icon">
+        <div
+            className="icon"
+        >
             <img src={src} alt="icon" />
         </div>
     );
