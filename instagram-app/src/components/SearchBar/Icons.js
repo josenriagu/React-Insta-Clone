ab8022f7fae981667b0febea998fbe6724b3d356
Josemaria <josemarianriagu25@gmail.com>
2019-06-06 19:40:50 +0800

    Implement toggle likeStatus

--- instagram-app/src/components/SearchBar/Icons.js
+++ instagram-app/src/components/SearchBar/Icons.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './SearchBar.css';
 
-const Icons = ({ src, name, updateLike }) => {
+const likedSrc = "/img/icons/like_alt.png";
+
+const Icons = ({ id, likeStatus, src, name, updateLike }) => {
     if (name === "like") {
         return (
             <div
+                id={id}
                 className="icon"
                 onClick={(event) => updateLike(event)}
             >
-                <img src={src} alt="icon" />
+                <img src={likeStatus ? likedSrc : src} alt="icon" />
             </div>)
     }
     return (
         <div
+            id={id}            
             className="icon"
         >
             <img src={src} alt="icon" />
