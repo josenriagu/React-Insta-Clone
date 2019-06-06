e36993a0823db12f2a87d57e20c028405771fb7e
Josemaria <josemarianriagu25@gmail.com>
2019-06-07 00:31:50 +0800

    Fix broken profile thumbnail url

--- instagram-app/src/components/ProfileSection/ProfileSection.js
+++ instagram-app/src/components/ProfileSection/ProfileSection.js
@@ -12,7 +12,7 @@ const ProfileSection = props => {
                             ?
                             <ProfileThumbnail src="https://scontent-los2-1.cdninstagram.com/vp/69f1ccde7a7e4a26e051822e0f230774/5D9D491E/t51.2885-19/s150x150/56358356_232738404265808_6854026862427897856_n.jpg?_nc_ht=scontent-los2-1.cdninstagram.com" alt="your profile thumbnail" />
                             :
-                            <ProfileThumbnail src="/ProfileThumbnail/icons/placeholder.jpg" alt="user profile thumbnail" />
+                            <ProfileThumbnail src="/img/icons/placeholder.jpg" alt="user profile thumbnail" />
                     }
                     }
                 </ProfileThumbnailDiv>
